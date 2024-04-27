283762ce0566dce8dee1e0712fc53d16282ddad7
nakagawa333 <naka33321@gmail.com>
2024-04-27 11:15:39 +0900

    このサイトについてページ修正

--- src/app/ui/components/Footer/footer.tsx
+++ src/app/ui/components/Footer/footer.tsx
@@ -3,9 +3,6 @@ export const Footer = () => {
         <>
           <footer>
             <nav className="bg-white border-gray-100 px-4 lg:px-6 py-2.5 dark:bg-gray-100">
-                <div>
-
-                </div>
             </nav>
           </footer>
         </>
