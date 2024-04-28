92d2753521c2c6fb3847ae16be7a9c7a821bdf3b
nakagawa333 <naka33321@gmail.com>
2024-04-29 04:14:07 +0900

    連絡先画面 バリデーションチェックの追加

--- src/app/ui/components/Contact/contact.tsx
+++ src/app/ui/components/Contact/contact.tsx
@@ -5,13 +5,68 @@ import { Header } from "../Header/header"
 /** 連絡先 */
 export const Contact = () => {
     const nameInputRef = useRef<HTMLInputElement>(null);
-    const mailInputRef = useRef<HTMLInputElement>(null);
+    const emailInputRef = useRef<HTMLInputElement>(null);
     const contactInformationRef = useRef<HTMLTextAreaElement>(null);
+
+    const nameMaxLength:number = 50;
+    const emailMaxLength:number = 50
+    const mailPattern:RegExp = /^[a-zA-Z0-9_+-]+(.[a-zA-Z0-9_+-]+)*@([a-zA-Z0-9][a-zA-Z0-9-]*[a-zA-Z0-9]*\.)+[a-zA-Z]{2,}$/
+
+    /**
+     * 文字が空文字であるかを判定する
+     * @param str 文字列
+     * @returns true:空文字 false:それ以外
+     */
+    const isEmpty = (str:string | null | undefined) => {
+        return str === "" || str === null || str === undefined;
+    }
 
     //送信ボタンクリック
     const submitClick = (e:any) => {
+        let validateCheckRes:boolean = validateCheck();
+        if(validateCheckRes === false){
+            return;   
+        }
         e.preventDefault();
-        alert("送信しました!")
+        alert("送信しました!");
+        //フォームの初期化
+        initForm();
+    }
+
+    const validateCheck = () => {
+        let name:any = nameInputRef?.current?.value;
+        if(isEmpty(name) || nameMaxLength < name?.length){
+            return false;
+        }
+
+        let email:any = emailInputRef?.current?.value;
+
+        if(isEmpty(email) || emailMaxLength < email.length){
+            return false;
+        }
+
+        let mailRegex = new RegExp(mailPattern);
+        if(mailRegex.test(email) === false){
+            return false;
+        }
+
+        let contactInformation:string | undefined = contactInformationRef?.current?.value;
+        if(isEmpty(contactInformation)){
+            return false;
+        }
+        return true;
+    }
+
+    const initForm = () => {
+        if(nameInputRef.current){
+            nameInputRef.current.value = "";
+        }
+        if(emailInputRef.current){
+            emailInputRef.current.value = "";
+        }
+        if(contactInformationRef.current){
+            contactInformationRef.current.value = "";
+        }
     }
 
     return (
@@ -30,8 +85,9 @@ export const Contact = () => {
                                   type="text" 
                                   id="subject" 
                                   className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-100 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
-                                  placeholder="お名前" 
-                                  required
+                                  placeholder="お名前"
+                                  maxLength={nameMaxLength}
+                                  required={true}
                                   ref={nameInputRef}
                                   />
                             </div>
@@ -42,8 +98,9 @@ export const Contact = () => {
                                    id="email"
                                    className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-100 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light" 
                                    placeholder="example@gmail.com" 
-                                   ref={mailInputRef}
-                                   required />
+                                   ref={emailInputRef}
+                                   required={true}
+                                />
                             </div>
                             <div className="sm:col-span-2">
                                 <label htmlFor="contactInformation" className="block mb-2 text-sm font-medium text-gray-900 dark:text-dark">お問い合わせ</label>
@@ -53,6 +110,7 @@ export const Contact = () => {
                                   className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-100 dark:border-gray-600 dark:placeholder-gray-400 dark:text-black dark:focus:ring-primary-500 dark:focus:border-primary-500" 
                                   placeholder="お問い合わせ内容"
                                   ref={contactInformationRef}
+                                  required={true}
                                 >
                                 
                                  </textarea>
